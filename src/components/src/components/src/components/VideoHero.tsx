00e8ba86115e454244d155bb859990a938705e78
swiss1001 <gvcopy10@gmail.com>
2025-06-24 16:03:22 -0300

    Create VideoHero.tsx

---
+++ src/components/src/components/src/components/VideoHero.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+
+const VideoHero = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://player.vturb.com.br/player.js';
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div style={{ width: '100%', maxWidth: '720px', margin: '0 auto' }}>
+      <vturb-smartplayer
+        data-id="vid_65f3bf5b5448bc00082f1aef"
+        style={{ width: '100%', height: '400px' }}
+      ></vturb-smartplayer>
+    </div>
+  );
+};
+
+export default VideoHero;
