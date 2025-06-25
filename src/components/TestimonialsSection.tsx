ab2b100aaca6a9e057a8481f21d1176c6394e55d
Gabriel Viera <gabrielviera@MacBook-Air-de-Gabriel-2.local>
2025-06-25 18:00:31 -0300

    Atualiza imagem do depoimento da Marina Costa com nova foto da Unsplash

--- src/components/TestimonialsSection.tsx
+++ src/components/TestimonialsSection.tsx
@@ -13,7 +13,7 @@ const TestimonialsSection = () => {
       name: "Marina Costa",
       age: 41,
       text: "O Clube das Pilateiras mudou minha rotina completamente. Agora tenho energia para tudo!",
-      image: "https://images.unsplash.com/photo-1494790108755-2616b612b786?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+      image: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&auto=format&fit=crop&w=870&q=80"
     },
     {
       name: "Juliana Santos",
